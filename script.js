92e41dff4e32b94f3217a716b71a8206ef9db090
kazspace <89419926+kazspace@users.noreply.github.com>
2024-12-01 16:37:31 +0000

    form handling, JS structure and plugin

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem", {
+    strings: "roses are red, violets are blue",
+    autoStart: true,
+    delay: 1,
+    cusor: "",
+  });
+}
+
+let poemForm = document.querySelector("#poem-gen-form");
+poemForm.addEventListener("submit", generatePoem);
