33e90a69d2822bd34fb3079ad53dab159c7e899e
kazspace <89419926+kazspace@users.noreply.github.com>
2024-12-04 18:29:32 +0000

    update message in footer

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function generatePoem(event) {
   let apiKey = "73f0d06a58bdbf0dee0944t7de9b9o05";
   let prompt = `User instructions: Generate a funny poem about ${instructionsInput.value}`;
   let context =
-    "You are a funny poet expert and love to write short poems. Your mission is to generate a 4 using line poem separating each line in basic HTML with <br /> - do not mention html or have a title. Make sure to follow the User instructions. At the end of the poem, sign off with 'SheCodes AI' inside a <strong> element";
+    "You are a funny poet expert and love to write short poems. Your mission is to generate a 4 using line poem separating each line in basic HTML with <br /> - do not mention html or have a title. Make sure to follow the User instructions. At the end of the poem, sign off with 'Powered by AI' inside a <strong> element";
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   let poemElement = document.querySelector("#poem");
